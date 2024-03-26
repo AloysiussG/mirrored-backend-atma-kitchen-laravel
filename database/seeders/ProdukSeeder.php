307fe536e7d3cdcfba80ab72a8177546f8c200ad
AloysiussG <aloysiusgsetog@gmail.com>
2024-03-26 07:46:26 +0700

    revision for 1/2 loyang in produk seeder

--- database/seeders/ProdukSeeder.php
+++ database/seeders/ProdukSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; 
+use Illuminate\Support\Facades\DB;
 
 class ProdukSeeder extends Seeder
 {
@@ -13,105 +13,149 @@ class ProdukSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('produks')->insert([[
-            //id =  1
-            'kategori_produk_id' => 1,
-            'penitip_id' =>null,
-            'nama_produk' => 'lapis legit',
-            'jumlah_stock' => null,
-            'status' => 'Pre Order',
-            'harga' => 850000,
-            'harga_setengah' => 450000,
-            'kuota_harian' => 10,
-        ],
-        [
-            //id =  2
-            'kategori_produk_id' => 1,
-            'penitip_id' =>null,
-            'nama_produk' => 'lapis surabaya',
-            'jumlah_stock' => 1,
-            'status' => 'Ready Stock',
-            'harga' => 550000,
-            'harga_setengah' => 300000,
-            'kuota_harian' => 10,
-        ],
-        [
-            //id =  3
-            'kategori_produk_id' => 1,
-            'penitip_id' =>null,
-            'nama_produk' => 'brownies',
-            'jumlah_stock' => null,
-            'status' => 'Pre Order',
-            'harga' => 250000,
-            'harga_setengah' => 150000,
-            'kuota_harian' => 10,
-        ],
-        [
-            //id =  4
-            'kategori_produk_id' => 1,
-            'penitip_id' =>null,
-            'nama_produk' => 'spikoe',
-            'jumlah_stock' => null,
-            'status' => 'Pre Order',
-            'harga' => 350000,
-            'harga_setengah' => 200000,
-            'kuota_harian' => 10,
-        ],
-        [
-            //id =  5
-            'kategori_produk_id' => 2,
-            'penitip_id' =>null,
-            'nama_produk' => 'roti sosis',
-            'jumlah_stock' => null,
-            'status' => 'Pre Order',
-            'harga' => 180000,
-            'harga_setengah' => null,
-            'kuota_harian' => 20,
-        ],
-        [
-            //id =  6
-            'kategori_produk_id' => 2,
-            'penitip_id' =>null,
-            'nama_produk' => 'milk bun',
-            'jumlah_stock' => 1,
-            'status' => 'Ready Stock',
-            'harga' => 120000,
-            'harga_setengah' => null,
-            'kuota_harian' => 20,
-        ],
-        [
-            //id =  7
-            'kategori_produk_id' => 3,
-            'penitip_id' =>null,
-            'nama_produk' => 'matcha creamy latte',
-            'jumlah_stock' => 0,
-            'status' => 'Pre Order',
-            'harga' => 100000,
-            'harga_setengah' => null,
-            'kuota_harian' => 20,
-        ],
-        [
-            //id =  8
-            'kategori_produk_id' => 4,
-            'penitip_id' =>1,
-            'nama_produk' => 'keripik kentang 250gr',
-            'jumlah_stock' => 10,
-            'status' => 'Ready Stock',
-            'harga' => 75000,
-            'harga_setengah' => null,
-            'kuota_harian' => 10,
-        ],
-        [
-            //id =  9
-            'kategori_produk_id' => 4,
-            'penitip_id' =>2,
-            'nama_produk' => 'Kopi Luwak Bubuk 250gr',
-            'jumlah_stock' => 10,
-            'status' => 'Ready Stock',
-            'harga' => 250000,
-            'harga_setengah' => null,
-            'kuota_harian' => 10,
-        ],
+        DB::table('produks')->insert([
+            [
+                //id =  1
+                'kategori_produk_id' => 1,
+                'penitip_id' => null,
+                'nama_produk' => 'Lapis Legit 1 Loyang',
+                'jumlah_stock' => null,
+                'status' => 'Pre Order',
+                'harga' => 850000,
+                // 'harga_setengah' => 450000,
+                'kuota_harian' => 10,
+            ],
+            [
+                'kategori_produk_id' => 1,
+                'penitip_id' => null,
+                'nama_produk' => 'Lapis Legit 1/2 Loyang',
+                'jumlah_stock' => 1,
+                'status' => 'Ready Stock',
+                'harga' => 450000,
+                // 'harga_setengah' => null,
+                'kuota_harian' => 10,
+            ],
+            [
+                //id =  2
+                'kategori_produk_id' => 1,
+                'penitip_id' => null,
+                'nama_produk' => 'Lapis Surabaya 1 Loyang',
+                'jumlah_stock' => 1,
+                'status' => 'Ready Stock',
+                'harga' => 550000,
+                // 'harga_setengah' => 300000,
+                'kuota_harian' => 10,
+            ],
+            [
+                //id =  2
+                'kategori_produk_id' => 1,
+                'penitip_id' => null,
+                'nama_produk' => 'Lapis Surabaya 1/2 Loyang',
+                'jumlah_stock' => 1,
+                'status' => 'Ready Stock',
+                'harga' => 300000,
+                // 'harga_setengah' => 300000,
+                'kuota_harian' => 10,
+            ],
+            [
+                //id =  3
+                'kategori_produk_id' => 1,
+                'penitip_id' => null,
+                'nama_produk' => 'Brownies 1 Loyang',
+                'jumlah_stock' => null,
+                'status' => 'Pre Order',
+                'harga' => 250000,
+                // 'harga_setengah' => 150000,
+                'kuota_harian' => 10,
+            ],
+            [
+                //id =  3
+                'kategori_produk_id' => 1,
+                'penitip_id' => null,
+                'nama_produk' => 'Brownies 1/2 Loyang',
+                'jumlah_stock' => null,
+                'status' => 'Pre Order',
+                'harga' => 150000,
+                // 'harga_setengah' => 150000,
+                'kuota_harian' => 10,
+            ],
+            [
+                //id =  4
+                'kategori_produk_id' => 1,
+                'penitip_id' => null,
+                'nama_produk' => 'Spikoe 1 Loyang',
+                'jumlah_stock' => null,
+                'status' => 'Pre Order',
+                'harga' => 350000,
+                // 'harga_setengah' => 200000,
+                'kuota_harian' => 10,
+            ],
+            [
+                //id =  4
+                'kategori_produk_id' => 1,
+                'penitip_id' => null,
+                'nama_produk' => 'Spikoe 1/2 Loyang',
+                'jumlah_stock' => null,
+                'status' => 'Pre Order',
+                'harga' => 200000,
+                // 'harga_setengah' => 200000,
+                'kuota_harian' => 10,
+            ],
+            [
+                //id =  5
+                'kategori_produk_id' => 2,
+                'penitip_id' => null,
+                'nama_produk' => 'Roti Sosis',
+                'jumlah_stock' => null,
+                'status' => 'Pre Order',
+                'harga' => 180000,
+                // 'harga_setengah' => null,
+                'kuota_harian' => 20,
+            ],
+            [
+                //id =  6
+                'kategori_produk_id' => 2,
+                'penitip_id' => null,
+                'nama_produk' => 'Milk Bun',
+                'jumlah_stock' => 1,
+                'status' => 'Ready Stock',
+                'harga' => 120000,
+                // 'harga_setengah' => null,
+                'kuota_harian' => 20,
+            ],
+            [
+                //id =  7
+                'kategori_produk_id' => 3,
+                'penitip_id' => null,
+                'nama_produk' => 'Matcha Creamy Latte',
+                'jumlah_stock' => 0,
+                'status' => 'Pre Order',
+                'harga' => 100000,
+                // 'harga_setengah' => null,
+                'kuota_harian' => 20,
+            ],
+            [
+                //id =  8
+                'kategori_produk_id' => 4,
+                'penitip_id' => 1,
+                'nama_produk' => 'Keripik Kentang 250gr',
+                'jumlah_stock' => 10,
+                'status' => 'Ready Stock',
+                'harga' => 75000,
+                // 'harga_setengah' => null,
+                'kuota_harian' => 10,
+            ],
+            [
+                //id =  9
+                'kategori_produk_id' => 4,
+                'penitip_id' => 2,
+                'nama_produk' => 'Kopi Luwak Bubuk 250gr',
+                'jumlah_stock' => 10,
+                'status' => 'Ready Stock',
+                'harga' => 250000,
+                // 'harga_setengah' => null,
+                'kuota_harian' => 10,
+            ],
         ]);
     }
 }
