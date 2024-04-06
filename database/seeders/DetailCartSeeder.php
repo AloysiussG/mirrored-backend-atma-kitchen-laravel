0794d033c2349a5b298698fd6cbb26dd426d1ed6
AloysiussG <aloysiusgsetog@gmail.com>
2024-04-06 11:40:36 +0700

    fix detail cart seeder in sync with dummy UTS

--- database/seeders/DetailCartSeeder.php
+++ database/seeders/DetailCartSeeder.php
@@ -15,84 +15,94 @@ class DetailCartSeeder extends Seeder
     {
         DB::table('detail_carts')->insert([
             [
-                //id = 1
-                'cart_id' => 1,
-                'produk_id' => 1,
-                'hampers_id' => null,
-                'jumlah' => 2,
-                'harga_produk_sekarang' => 850000,
+                'cart_id' => '1',
+                'produk_id' => '1',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '850000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 2
-                'cart_id' => 1,
-                'produk_id' => 12,
-                'hampers_id' => null,
-                'jumlah' => 3,
-                'harga_produk_sekarang' => 250000,
+                'cart_id' => '1',
+                'produk_id' => '13',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '250000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 3
-                'cart_id' => 3,
-                'produk_id' => 2,
-                'hampers_id' => null,
-                'jumlah' => 1,
-                'harga_produk_sekarang' => 300000,
+                'cart_id' => '3',
+                'produk_id' => '4',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '300000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 4
-                'cart_id' => 3,
-                'produk_id' => 7,
-                'hampers_id' => null,
-                'jumlah' => 1,
-                'harga_produk_sekarang' => 100000,
+                'cart_id' => '3',
+                'produk_id' => '11',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '100000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 5
-                'cart_id' => 4,
-                'produk_id' => 3,
-                'hampers_id' => null,
-                'jumlah' => 1,
-                'harga_produk_sekarang' => 250000,
+                'cart_id' => '4',
+                'produk_id' => '5',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '250000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 6
-                'cart_id' => 4,
-                'produk_id' => 6,
-                'hampers_id' => null,
-                'jumlah' => 1,
-                'harga_produk_sekarang' => 120000,
+                'cart_id' => '4',
+                'produk_id' => '10',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '120000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 7
-                'cart_id' => 5,
-                'produk_id' => 4,
-                'hampers_id' => null,
-                'jumlah' => 1,
-                'harga_produk_sekarang' => 200000,
+                'cart_id' => '5',
+                'produk_id' => '8',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '200000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 8
-                'cart_id' => 5,
-                'produk_id' => 5,
-                'hampers_id' => null,
-                'jumlah' => 1,
-                'harga_produk_sekarang' => 180000,
+                'cart_id' => '5',
+                'produk_id' => '9',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '180000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 9
-                'cart_id' => 5,
-                'produk_id' => 13,
-                'hampers_id' => null,
-                'jumlah' => 2,
-                'harga_produk_sekarang' => 75000,
+                'cart_id' => '6',
+                'produk_id' => '12',
+                'hampers_id' => NULL,
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '75000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
             [
-                //id = 10
-                'cart_id' => 5,
-                'produk_id' => null,
-                'hampers_id' => 1,
-                'jumlah' => 1,
-                'harga_produk_sekarang' => 650000,
+                'cart_id' => '6',
+                'produk_id' => NULL,
+                'hampers_id' => '1',
+                'jumlah' => '1',
+                'harga_produk_sekarang' => '650000',
+                'created_at' => NULL,
+                'updated_at' => NULL
             ],
         ]);
     }
