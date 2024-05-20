18b5d7a5ddfa86781bb5892f9f977e178ab75234
AloysiussG <aloysiusgsetog@gmail.com>
2024-05-20 17:55:12 +0700

    add cek jumlah ready stock & sisa kuota harian, 2 recaps, add warnings (for tanggal ambil also), allowed status

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -106,24 +106,76 @@ class CartController extends Controller
                 );
             }
 
+            // CEK JUMLAH STOCK (READY STOCK) & CEK KUOTA HARIAN (PRE ORDER)
+
             // get seluruh rekap produk di cart
+            $allProduksCartReadyStock = [];
             $allProduksCart = [];
+            $warnings = 0;
+
             $newArr = [];
 
             foreach ($cart->detailCart as $detailItem) {
                 if (isset($detailItem->produk)) {
-                    $newArr = $detailItem['produk'];
-                    $newArr['jumlah'] = $detailItem['jumlah'];
-                    $allProduksCart[] = $newArr;
+                    if ($detailItem->status_produk === 'Ready Stock') {
+                        $newArr = $detailItem['produk'];
+                        $newArr['jumlah'] = $detailItem['jumlah'];
+                        $allProduksCartReadyStock[] = $newArr;
+                    } else {
+                        $newArr = $detailItem['produk'];
+                        $newArr['jumlah'] = $detailItem['jumlah'];
+                        $allProduksCart[] = $newArr;
+                    }
                 } else if (isset($detailItem->hampers)) {
                     foreach ($detailItem->hampers->detailHampers as $detailHampers) {
-                        $newArr = $detailHampers['produk'];
-                        $newArr['jumlah'] = $detailItem['jumlah'] * $detailHampers['jumlah_produk'];
-                        $allProduksCart[] = $newArr;
+                        if ($detailItem->status_produk === 'Ready Stock') {
+                            $newArr = $detailHampers['produk'];
+                            $newArr['jumlah'] = $detailItem['jumlah'] * $detailHampers['jumlah_produk'];
+                            $allProduksCartReadyStock[] = $newArr;
+                        } else {
+                            $newArr = $detailHampers['produk'];
+                            $newArr['jumlah'] = $detailItem['jumlah'] * $detailHampers['jumlah_produk'];
+                            $allProduksCart[] = $newArr;
+                        }
                     }
                 }
             }
 
+            // ------------------ khusus ready stock ------------------
+
+            // ready stock collection
+            $allProduksCartReadyStockCollection = collect($allProduksCartReadyStock);
+            $groupedAllProduksReadyStock = $allProduksCartReadyStockCollection->groupBy('id');
+            $allProduksCartReadyStockResults = $groupedAllProduksReadyStock->map(function ($group) {
+                $firstItem = $group->first();
+                $firstItem['jumlah'] = $group->sum('jumlah');
+                return $firstItem;
+            })->values();
+
+            // compare ready stock collection & stok
+            $allWithJumlahStock = $allProduksCartReadyStockResults->map(function ($itemProduk) use (&$warnings) {
+                // jika jumlah beli melebihi stok
+                if ($itemProduk['jumlah'] > $itemProduk['jumlah_stock']) {
+                    $itemProduk['warning'] = 'Pesanan melebihi stok';
+                    $warnings = $warnings + 1;
+                }
+
+                return $itemProduk;
+            });
+
+            $cart['recap_by_produk_ready_stock'] = $allWithJumlahStock;
+
+
+
+
+            // -----------------------------------------------------------------------------------------------------
+
+
+
+
+            // ------------------ khusus kuota harian ------------------
+
+            // kuota harian collection
             $allProduksCartCollection = collect($allProduksCart);
             $groupedAllProduks = $allProduksCartCollection->groupBy('id');
             $allProduksCartResults = $groupedAllProduks->map(function ($group) {
@@ -131,8 +183,6 @@ class CartController extends Controller
                 $firstItem['jumlah'] = $group->sum('jumlah');
                 return $firstItem;
             })->values();
-
-            // ------------------
 
             // get produk di banyak transaksi pada tanggal ambil
             $allProduksTransaksi = [];
@@ -144,18 +194,23 @@ class CartController extends Controller
                 ->whereNotIn('status_transaksi_id', [5, 12])
                 ->get();
 
+            // cari transaksi yang pre order aja (karena berkaitan dengan kuota harian)
             foreach ($transaksiArr as $value) {
                 $detailCarts = $value->cart->detailCart;
                 foreach ($detailCarts as $detailItem) {
                     if (isset($detailItem->produk)) {
-                        $newArr['id'] = $detailItem['produk']['id'];
-                        $newArr['jumlah_dibeli'] = $detailItem['jumlah'];
-                        $allProduksTransaksi[] = $newArr;
+                        if ($detailItem->status_produk === 'Pre Order') {
+                            $newArr['id'] = $detailItem['produk']['id'];
+                            $newArr['jumlah_dibeli'] = $detailItem['jumlah'];
+                            $allProduksTransaksi[] = $newArr;
+                        }
                     } else if (isset($detailItem->hampers)) {
                         foreach ($detailItem->hampers->detailHampers as $detailHampers) {
-                            $newArr['id'] = $detailHampers['produk']['id'];
-                            $newArr['jumlah_dibeli'] = $detailItem['jumlah'] * $detailHampers['jumlah_produk'];
-                            $allProduksTransaksi[] = $newArr;
+                            if ($detailItem->status_produk === 'Pre Order') {
+                                $newArr['id'] = $detailHampers['produk']['id'];
+                                $newArr['jumlah_dibeli'] = $detailItem['jumlah'] * $detailHampers['jumlah_produk'];
+                                $allProduksTransaksi[] = $newArr;
+                            }
                         }
                     }
                 }
@@ -171,7 +226,8 @@ class CartController extends Controller
                 ];
             })->values();
 
-            $allWithSisaKuota = $allProduksCartResults->map(function ($itemProduk) use ($allProduksTransaksiResults, $cart) {
+            // map all produks cart collection & compare dengan collection transaksi pada tanggal ambil
+            $allWithSisaKuota = $allProduksCartResults->map(function ($itemProduk) use ($allProduksTransaksiResults, &$warnings) {
                 $jumlahDibeli = 0;
                 $found = $allProduksTransaksiResults->firstWhere('id', $itemProduk->id);
                 if ($found) {
@@ -185,8 +241,8 @@ class CartController extends Controller
 
                 // jika jumlah beli melebihi sisa kuota
                 if ($itemProduk->jumlah > $sisaKuota) {
-                    $itemProduk['warning'] = 'Pesanan melebihi kuota';
-                    $cart['has_warning'] = true;
+                    $itemProduk['warning'] = 'Pesanan melebihi sisa kuota';
+                    $warnings = $warnings + 1;
                 }
 
                 $itemProduk['jumlah_pembelian_pada_tanggal_ambil'] = $jumlahDibeli;
@@ -194,7 +250,117 @@ class CartController extends Controller
                 return $itemProduk;
             });
 
-            $cart['recap_by_produk'] = $allWithSisaKuota;
+            $cart['recap_by_produk_kuota_harian'] = $allWithSisaKuota;
+
+            $length = count($cart['recap_by_produk_kuota_harian']);
+            if ($length) {
+                $nowPlus2 = Carbon::now()->addDays(2)->format('Y-m-d');
+                $nowPlus2 = Carbon::parse($nowPlus2);
+                $tglAmbilParsed = Carbon::parse($request->tanggal_ambil);
+
+                if (!$tglAmbilParsed->greaterThanOrEqualTo($nowPlus2)) {
+                    $warnings = $warnings + 1;
+                    $cart['warning_min_date'] = $nowPlus2->format('Y-m-d');
+                }
+            }
+
+            $cart['warnings_count'] = $warnings;
+
+
+
+
+
+            // // CEK KUOTA HARIAN (PO)
+
+            // // get seluruh rekap produk di cart
+            // $allProduksCart = [];
+            // $newArr = [];
+
+            // foreach ($cart->detailCart as $detailItem) {
+            //     if (isset($detailItem->produk)) {
+            //         $newArr = $detailItem['produk'];
+            //         $newArr['jumlah'] = $detailItem['jumlah'];
+            //         $allProduksCart[] = $newArr;
+            //     } else if (isset($detailItem->hampers)) {
+            //         foreach ($detailItem->hampers->detailHampers as $detailHampers) {
+            //             $newArr = $detailHampers['produk'];
+            //             $newArr['jumlah'] = $detailItem['jumlah'] * $detailHampers['jumlah_produk'];
+            //             $allProduksCart[] = $newArr;
+            //         }
+            //     }
+            // }
+
+            // $allProduksCartCollection = collect($allProduksCart);
+            // $groupedAllProduks = $allProduksCartCollection->groupBy('id');
+            // $allProduksCartResults = $groupedAllProduks->map(function ($group) {
+            //     $firstItem = $group->first();
+            //     $firstItem['jumlah'] = $group->sum('jumlah');
+            //     return $firstItem;
+            // })->values();
+
+            // // ------------------
+
+            // // get produk di banyak transaksi pada tanggal ambil
+            // $allProduksTransaksi = [];
+            // $newArr = [];
+
+            // $transaksiArr = Transaksi::query()
+            //     ->with(['cart.detailCart.produk.kategoriProduk', 'cart.detailCart.hampers.detailHampers.produk.kategoriProduk'])
+            //     ->where('tanggal_ambil', $request->tanggal_ambil)
+            //     ->whereNotIn('status_transaksi_id', [5, 12])
+            //     ->get();
+
+            // foreach ($transaksiArr as $value) {
+            //     $detailCarts = $value->cart->detailCart;
+            //     foreach ($detailCarts as $detailItem) {
+            //         if (isset($detailItem->produk)) {
+            //             $newArr['id'] = $detailItem['produk']['id'];
+            //             $newArr['jumlah_dibeli'] = $detailItem['jumlah'];
+            //             $allProduksTransaksi[] = $newArr;
+            //         } else if (isset($detailItem->hampers)) {
+            //             foreach ($detailItem->hampers->detailHampers as $detailHampers) {
+            //                 $newArr['id'] = $detailHampers['produk']['id'];
+            //                 $newArr['jumlah_dibeli'] = $detailItem['jumlah'] * $detailHampers['jumlah_produk'];
+            //                 $allProduksTransaksi[] = $newArr;
+            //             }
+            //         }
+            //     }
+            // }
+
+            // $collection = collect($allProduksTransaksi);
+            // $grouped = $collection->groupBy('id');
+
+            // $allProduksTransaksiResults = $grouped->map(function ($group, $id) {
+            //     return [
+            //         'id' => $id,
+            //         'jumlah_dibeli' => $group->sum('jumlah_dibeli')
+            //     ];
+            // })->values();
+
+            // $allWithSisaKuota = $allProduksCartResults->map(function ($itemProduk) use ($allProduksTransaksiResults, $cart) {
+            //     $jumlahDibeli = 0;
+            //     $found = $allProduksTransaksiResults->firstWhere('id', $itemProduk->id);
+            //     if ($found) {
+            //         $jumlahDibeli = $found['jumlah_dibeli'];
+            //     }
+
+            //     $sisaKuota = $itemProduk->kuota_harian - $jumlahDibeli;
+            //     if ($sisaKuota < 0) {
+            //         $sisaKuota = 0;
+            //     }
+
+            //     // jika jumlah beli melebihi sisa kuota
+            //     if ($itemProduk->jumlah > $sisaKuota) {
+            //         $itemProduk['warning'] = 'Pesanan melebihi kuota';
+            //         $cart['has_warning'] = true;
+            //     }
+
+            //     $itemProduk['jumlah_pembelian_pada_tanggal_ambil'] = $jumlahDibeli;
+            //     $itemProduk['sisa_kuota_harian'] = $sisaKuota;
+            //     return $itemProduk;
+            // });
+
+            // $cart['recap_by_produk'] = $allWithSisaKuota;
 
             return response()->json(
                 [
@@ -317,6 +483,38 @@ class CartController extends Controller
             }
 
             $cart['poin_didapat'] = $poinDidapat;
+
+
+
+            $detailCartsModified = $cart->detailCart->map(function ($item) {
+                if (isset($item->produk)) {
+                    if ($item->produk->status === 'Pre Order') {
+                        // hanya bisa PO ONLY
+                        $item['arr_allowed_status'] = ['Pre Order'];
+                    } else if ($item->produk->status === 'Ready Stock') {
+                        if ($item->produk->kategoriProduk->nama_kategori_produk === 'Titipan') {
+                            // hanya bisa READY STOCK ONLY
+                            $item['arr_allowed_status'] = ['Ready Stock'];
+                        } else {
+                            // bisa PO & READY STOCK
+                            $item['arr_allowed_status'] = ['Pre Order', 'Ready Stock'];
+                        }
+                    }
+                } else if (isset($item->hampers)) {
+                    $found = $item->hampers->detailHampers->firstWhere('produk.status', 'Pre Order');
+                    if ($found) {
+                        // hanya bisa PO ONLY
+                        $item['arr_allowed_status'] = ['Pre Order'];
+                    } else {
+                        // bisa PO & READY STOCK
+                        $item['arr_allowed_status'] = ['Pre Order', 'Ready Stock'];
+                    }
+                }
+
+                return $item;
+            });
+
+            $cart['detail_cart_modified'] = $detailCartsModified;
 
             // KALO ACTIVE CART TIDAK DITEMUKAN
             // TODO::: bikin cart baru ???
